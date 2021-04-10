00e7275600bd3c233c5e62ed81b8bc1edf554a70
remote-torrent-6747 <abbyc@sky.com>
2021-04-10 12:26:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,9 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get "/reversename/:name" do
-    @user_name = param[:name]
-    @name_reverse = @user_name.reverse
-    @name_reverse
+    param[:name].reverse
   end
 
 end
